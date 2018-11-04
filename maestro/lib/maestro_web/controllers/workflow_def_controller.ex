6de7ac59b4ff6aa52749c3072b24fa08b4cf7cb2
Steven Landow <greenpipestudios@gmail.com>
2018-11-04 01:25:12 -0500

    expose api for creating a workflow definition

--- maestro/lib/maestro_web/controllers/workflow_def_controller.ex
+++ maestro/lib/maestro_web/controllers/workflow_def_controller.ex
@@ -4,8 +4,14 @@ defmodule MaestroWeb.WorkflowDefController do
 
   def get_workflows(conn, _params), do: json(conn, WorkflowDefs.list_workflow_defs() |> IO.inspect)
 
-  def get_workflow(conn, %{"name" => name, "version" => version}),
-      do: json(conn, WorkflowDefs.get_workflow_def(name, version))
+
+  def get_workflow(conn, %{"name" => name, "version" => version}) do
+    case WorkflowDefs.get_workflow_def(name, version) do
+      {:ok, workflow_def} -> json(conn, workflow_def)
+      {:error, :not_found} -> put_status(conn, :not_found)
+                              |> json(%{"message" => "No workflow found with the name '#{name}' and version #{version}"})
+    end
+  end
 
   def get_workflow(conn, %{"name" => name}) do
     case WorkflowDefs.get_workflow_def(name) do
@@ -15,12 +21,13 @@ defmodule MaestroWeb.WorkflowDefController do
     end
   end
 
-  def get_workflow(conn, %{"name" => name, "version" => version}) do
-    case WorkflowDefs.get_workflow_def(name, version) do
-      {:ok, workflow_def} -> json(conn, workflow_def)
-      {:error, :not_found} -> put_status(conn, :not_found)
-                              |> json(%{"message" => "No workflow found with the name '#{name}' at version #{version}"})
+  def create_workflow(conn, params) do
+    case WorkflowDefs.create_workflow_def(params) do
+      {:ok, _} -> send_resp(conn, :no_content, "")
+      {:error, _} -> put_status(conn, :bad_request)
+                          |> json(%{"message" => "Failed to create workflow. Make sure the definition is valid and another workflow does not exist with the same name and version. "})
     end
   end
+
 
 end
--- maestro/lib/maestro_web/router.ex
+++ maestro/lib/maestro_web/router.ex
@@ -7,6 +7,7 @@ defmodule MaestroWeb.Router do
 
   scope "/api/metadata", MaestroWeb do
     pipe_through :api
+    post("/workflows", WorkflowDefController, :create_workflow)
     get("/workflows", WorkflowDefController, :get_workflows)
     get("/workflows/:name", WorkflowDefController, :get_workflow)
 
